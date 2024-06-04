e8e756b068d0e9e4ce6b36f81faa48c8f27caf47
Garyn14 <garyn.fernando.hr@gmail.com>
2024-06-03 21:22:51 -0500

    Commit de prueba

--- src/main/java/com/Alura/Main.java
+++ src/main/java/com/Alura/Main.java
@@ -2,6 +2,6 @@ package com.Alura;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hola mundo");
     }
 }
