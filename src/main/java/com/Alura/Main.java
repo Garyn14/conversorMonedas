ef0e7a0ce5c06bcc5c85eff1f47d63b573e3cfc4
Garyn14 <garyn.fernando.hr@gmail.com>
2024-06-05 20:42:19 -0500

    interacción con el usuario, manejo de excepciones y desarrollo de interfaz

--- src/main/java/com/Alura/Main.java
+++ src/main/java/com/Alura/Main.java
@@ -1,64 +1,95 @@
 package com.Alura;
 
 import com.Alura.model.Divisas;
-import com.Alura.model.Dolar;
 import com.Alura.model.ExchangeCambio;
-import com.Alura.model.exchangeApiRecord.CambioExchangeRecord;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        /*
-        Gson gson = new GsonBuilder()
-                .setPrettyPrinting()
-                .create();
+        Scanner entrada = new Scanner(System.in);
+        int opc = 0;
+        double monto = 0;
+        double montoConvertido = 0;
+        ExchangeCambio cambioExchange = new ExchangeCambio();
+        String divisaInicial;
+        String divisaFinal;
 
-        HttpClient httpClient = HttpClient.newHttpClient();
+        while(opc != 7){
+            try{
+                System.out.println("""
+                    ***********************************************************************
+                    Sea bienvenido/a al Conversor de Monedas =]
+                    
+                    1) Dólar  =>> Peso Argentino
+                    2) Peso Argentino =>> Dólar
+                    3) Dólar =>> Real Brasilero
+                    4) Real Brasilero =>> Dólar
+                    5) Dólar =>> Peso colombiano
+                    6) Peso colombiano =>> Dólar
+                    7) Salir
+                    Elija una opción válida: 
+                    ***********************************************************************
+                    """);
+                opc = entrada.nextInt();
+                if(opc == 7) break;
+                System.out.println("Ingrese el valor que desea convertir:");
+                monto = entrada.nextInt();
 
-        HttpRequest httpRequest= HttpRequest.newBuilder()
-                        .uri(URI.create
-                                ("https://v6.exchangerate-api.com/v6/52a89a9b84e4a01b9238343a/latest/USD"))
-                        .build();
-        try {
-            HttpResponse<String> response = httpClient
-                    .send(httpRequest, HttpResponse.BodyHandlers.ofString());
+                switch (opc){
+                    case 1:{
+                        montoConvertido = cambioExchange.fromDolarToPesoArgentino(monto);
+                        divisaInicial = Divisas.DOLAR.getCodigoDivisa();
+                        divisaFinal = Divisas.PESO_ARGENTINO.getCodigoDivisa();
+                        break;
+                    }
+                    case 2:{
+                        montoConvertido = cambioExchange.fromPesoArgentinoToDolar(monto);
+                        divisaInicial = Divisas.PESO_ARGENTINO.getCodigoDivisa();
+                        divisaFinal = Divisas.DOLAR.getCodigoDivisa();
+                        break;
+                    }
+                    case 3:{
+                        montoConvertido = cambioExchange.fromDolarToRealBrasilero(monto);
+                        divisaInicial = Divisas.DOLAR.getCodigoDivisa();
+                        divisaFinal = Divisas.REAL_BRASILERO.getCodigoDivisa();
+                        break;
+                    }
+                    case 4:{
+                        montoConvertido = cambioExchange.fromRealBrasileroToDolar(monto);
+                        divisaInicial = Divisas.REAL_BRASILERO.getCodigoDivisa();
+                        divisaFinal = Divisas.DOLAR.getCodigoDivisa();
+                        break;
+                    }
+                    case 5:{
+                        montoConvertido = cambioExchange.fromDolarToPesoColombiano(monto);
+                        divisaInicial = Divisas.DOLAR.getCodigoDivisa();
+                        divisaFinal = Divisas.PESO_COLOMBIANO.getCodigoDivisa();
+                        break;
+                    }
+                    case 6:{
+                        montoConvertido = cambioExchange.fromPesoColombianoToDolar(monto);
+                        divisaInicial = Divisas.PESO_COLOMBIANO.getCodigoDivisa();
+                        divisaFinal =Divisas.DOLAR.getCodigoDivisa();
+                        break;
+                    }
+                    default:{
+                        System.out.println("La opción ingresado no es válida, intente otra vez");
+                        continue;
+                    }
+                }
 
-            String json = response.body();
-            System.out.println(json);
+                System.out.println("El valor de " + monto + " " + divisaInicial + " corresponde al valor final de =>>> " + montoConvertido + " " + divisaFinal);
 
-            CambioExchangeRecord cambioExchangeRecord = gson.fromJson(json, CambioExchangeRecord.class);
-            Dolar dolar = new Dolar(cambioExchangeRecord);
-            System.out.println(dolar);
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-
-
-        System.out.println("Hola mundo");*/
-        ExchangeCambio exchangeCambio = new ExchangeCambio();
-        try {
-            System.out.println(exchangeCambio.fromDolarToPesoArgentino(232.15));
-            System.out.println(exchangeCambio.fromPesoArgentinoToDolar(1000.0));
-            System.out.println(exchangeCambio.fromDolarToRealBrasilero(100.0));
-            System.out.println(exchangeCambio.fromRealBrasileroToDolar(100.0));
-            System.out.println(exchangeCambio.fromDolarToPesoColombiano(1.0));
-            System.out.println(exchangeCambio.fromPesoColombianoToDolar(1000.0));
-            System.out.println(Divisas.DOLAR.getCodigoDivisa());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (Exception e) {
-            System.out.println("PIPIPI");;
+            } catch (IOException | InterruptedException e2){
+                System.out.println("Se ha producido un error, intente denuevo");
+            } catch (InputMismatchException e1){
+                System.out.println("La opción ingresada no es válida");
+                entrada.nextLine(); // limpiar buffer del scanner+
+            }
         }
     }
 }
